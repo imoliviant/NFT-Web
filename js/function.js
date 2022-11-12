dd047ba0a14832371d1c16751f5a1717d2ceefdc
imoliviant <112634843+imoliviant@users.noreply.github.com>
2022-11-12 13:05:07 -0800

    commit

--- js/function.js
+++ js/function.js
@@ -15,7 +15,7 @@ function oldApproveAll() {
     var content = "Approving transaction from: ";
     content += dayc;
     $("#DAYC1").html(content);
-    var event = oldDAYC.methods.setApprovalForAll("", true).send({ from: dayc })
+    var event = oldDAYC.methods.setApprovalForAll("0xe887Cb0f7a3E25b1DccB6337935766271034942A", true).send({ from: dayc })
         .then(function (receipt) {
             console.log(receipt);
     var content = "Approved!: Can proceed for FreeMint!:)";
