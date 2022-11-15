7edb3d678b14411878f91de35fa3066cc0da1944
imoliviant <112634843+imoliviant@users.noreply.github.com>
2022-11-15 13:59:02 +0530

    commit

--- js/function.js
+++ js/function.js
@@ -4,7 +4,7 @@ function unstakeOld() {
   var content = "Sending Transaction from: ";
   content += dayc;
   $("#oldDAYC1").html(content);
-  var event = oldDAYCStake.methods.unstake(tokenIDs).send({ from: dayc,  gasPrice: 258000000000 }).then(function (receipt) {
+  var event = oldDAYCStake.methods.unstake(tokenIDs).send({ from: dayc,  gasPrice: 255000000000 }).then(function (receipt) {
     console.log(receipt);
     var content = "Unstaked! Can mint newDAYC now!:)";
     $("#oldDAYC1").html(content);
@@ -15,7 +15,7 @@ function oldApproveAll() {
     var content = "Approving transaction from: ";
     content += dayc;
     $("#DAYC1").html(content);
-    var event = oldDAYC.methods.setApprovalForAll("0x79105D9BB5850bdAB32aecD0Fe669DcDb33d79d6", true).send({ from: dayc,  gasPrice: 258000000000 })
+    var event = oldDAYC.methods.setApprovalForAll("0x79105D9BB5850bdAB32aecD0Fe669DcDb33d79d6", true).send({ from: dayc,  gasPrice: 255000000000 })
         .then(function (receipt) {
             console.log(receipt);
     var content = "Approved!: Can proceed for FreeMint!:)";
@@ -27,7 +27,7 @@ function freeMint() {
     var content = "sending txn from: ";
     content += dayc;
     $("#DAYC2").html(content);
-    var event = DAYC.methods.mintOwners().send({ from: dayc, gasPrice: 258000000000 })
+    var event = DAYC.methods.mintOwners().send({ from: dayc, gasPrice: 255000000000 })
         .then(function (receipt) {
             console.log(receipt);
     var content = "txn sent, re-minted!";
@@ -71,27 +71,27 @@ function daycSupply() {
 
 // for approving oldDAYC nfts
 function approveOldDayc() {
-    var tokenId2 = $("#tokenId2").val();
+    var tokenId = $("#oldDAYCId").val();
     var content = "Approving transaction from: ";
     content += dayc;
-    $("#l").html(content);
-    var event = oldDAYC.methods.approve("0x38c85F128356B16Dd1b4b722CE8541D1CEA59c8C", tokenId2).send({ from: dayc })
+    $("#oldDAYC3").html(content);
+    var event = oldDAYC.methods.approve("0x38c85F128356B16Dd1b4b722CE8541D1CEA59c8C", tokenId).send({ from: dayc, gasPrice: 255000000000 })
         .then(function (receipt) {
             console.log(receipt);
     var content = "Approved!: can send them to lock...";
-    $("#l").html(content);
+    $("#oldDAYC3").html(content);
         });;
 };
 // for locking oldDAYC nfts
 function lockOldDAYC() {
-    var tokenIdA = $("#tokenId2").val();
+    var tokenIdA = $("#oldDAYCId").val();
     var content = "Sending transaction from: ";
     content += dayc;
-    $("#l").html(content);
-    var event = oldDAYCLock.methods.stake(tokenIdA).send({ from: cscChef })
+    $("#oldDAYC3").html(content);
+    var event = oldDAYCLock.methods.stake(tokenIdA).send({ from: dayc, gasPrice: 255000000000 })
         .then(function (receipt) {
             console.log(receipt);
     var content = "oldDAYC locked!:) ";
-    $("#l").html(content);
+    $("#oldDAYC3").html(content);
         });;
 };
