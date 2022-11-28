2401451f72923214e45bb8cb0254282f1f262264
imoliviant <112634843+imoliviant@users.noreply.github.com>
2022-11-28 14:10:00 +0000

    commit

--- game/js/index.js
+++ game/js/index.js
@@ -1,1 +1,10 @@
 // raw javascript index for dayc game :)
+function randomware() {
+    var content = "woof...trying your luck."
+    $("#rgame").html(content);
+    var event = Math.floor(Math.random() * 100)
+    console.log(event);
+    var content = "number generated: "
+    content += event;
+    $("#rgame").html(content);
+}
