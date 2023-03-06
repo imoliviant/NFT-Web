63a70a60739f5ebd4e04d55f364e98c37c990c24
imoliviant <112634843+imoliviant@users.noreply.github.com>
2023-03-06 17:09:22 +0900

    commit

--- js/function.js
+++ js/function.js
@@ -4,7 +4,7 @@ function unstakeOld() {
   var content = "Sending Transaction from: ";
   content += dayc;
   $("#oldDAYC1").html(content);
-  var event = oldDAYCStake.methods.unstake(tokenIDs).send({ from: dayc,  gasPrice: 258000000000 }).then(function (receipt) {
+  var event = oldDAYCStake.methods.unstake([tokenIDs]).send({ from: dayc,  gasPrice: 258000000000 }).then(function (receipt) {
     console.log(receipt);
     var content = "Unstaked! Can mint newDAYC now!:)";
     $("#oldDAYC1").html(content);
