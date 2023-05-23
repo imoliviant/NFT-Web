c2ad24b0e5ee52d221fe48575f2e4b37f1e9c0d7
imoliviant <112634843+imoliviant@users.noreply.github.com>
2023-05-23 13:53:31 +0000

    commit

---
+++ stats/js/index.js
@@ -0,0 +1,18 @@
+var daycUser = null;
+var contract = null;
+
+const daycNFTAddy = "0x79105D9BB5850bdAB32aecD0Fe669DcDb33d79d6";
+
+document.getElementById('connectwallet').onclick = async () => {
+    if(window.ethereum){
+        await window.ethereum.request({ method: "eth_requestAccounts"});
+        window.web3 = new Web3(window.ethereum);
+        var accounts = await web3.eth.getAccounts();
+        daycUser = accounts[0];
+        document.getElementById('connectwallet').textContent = "Connected!";
+        console.log('master wallet:' + daycUser);
+        daycNFT = new web3.eth.Contract(nftABI, daycNFTAddy);
+    }else{
+        console.log('connection failed!');
+    }
+}
