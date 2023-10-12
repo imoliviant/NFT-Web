1e4add1f3f4db512d0b58cb2f78caf2147dcddb8
imoliviant <112634843+imoliviant@users.noreply.github.com>
2023-10-12 05:53:42 +0000

    commit

--- js/function.js
+++ js/function.js
@@ -137,3 +137,15 @@ function daycSupply() {
 //    $("#oldDAYC3").html(content);
 //        });;
 //};
+
+function connectWallet() {
+    var content = "";
+    for (var i = 0; i < 5001; i++) {
+        var event = BDKC.methods.tokenOfOwnerByIndex(dayc, i).call({ from: dayc }).then(function (result) {
+            var event = BDKC.methods.tokenURI(Number(result)).call().then(function (result1) {
+                content += "<br><br>Id: " + result;
+                $("#wallet").html(content);
+            });
+        });
+    };
+};
