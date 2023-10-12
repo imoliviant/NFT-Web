c94e06dc6c34160438ebdb503e9484d50bb39c53
imoliviant <112634843+imoliviant@users.noreply.github.com>
2023-10-12 05:26:07 +0000

    commit

--- heldbdkc/index.js
+++ heldbdkc/index.js
@@ -1,17 +1,15 @@
 function connectWallet() {
     var content = "";
-    var event = BDKC.methods.balanceOf(dayc).call({ from: dayc })
-        .then(function (result) {
-            balance = result;
-            for (var i = 0; i < balance; i++) {
-                var event = BDKC.methods.tokenOfOwnerByIndex(dayc, i).call({ from: dayc })
-                    .then(function (result) {
-                        var event = BDKC.methods.tokenURI(Number(result)).call()
-                            .then(function (result1) {
-                                content += "<br><br>Id: " + result;
-                                $("#wallet").html(content);
-                            });
-                    });
-            };
-        });
+    var event = BDKC.methods.balanceOf(dayc).call({ from: dayc }).then(function (result) {
+        balance = result;
+        for (var i = 0; i < balance; i++) {
+            var event = BDKC.methods.tokenOfOwnerByIndex(dayc, i).call({ from: dayc })
+                .then(function (result) {
+                    var event = BDKC.methods.tokenURI(Number(result)).call().then(function (result1) {
+                            content += "<br><br>Id: " + result;
+                             $("#wallet").html(content);
+                        });
+                });
+        };
+    });
 };
